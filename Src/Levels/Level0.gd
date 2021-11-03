08872453fc6d193c112b36d7c741eddfa86f4a96
mago <markus.schneider84@gmail.com>
2021-11-03 12:30:20 +0100

    added basic movement

--- Src/Levels/Level0.gd
+++ Src/Levels/Level0.gd
@@ -1,7 +1,33 @@
 extends Node2D
 
+const GHOST_SPAWN_DELAY = 1.0
+
+var player_scene = preload("res://Src/Player/Player.tscn")
+
+var ghosts = []
 
 func _on_ButtonBack_button_up():
 	Events.emit_signal("play_sound", "menu_click")
 	Events.emit_signal("menu_back")
 
+
+
+func _ready():
+	spawn_player()
+
+
+func spawn_player(ghost_no = -1):
+	# Level must contain a spawn
+	assert($PlayerSpawn)
+	
+	var spawn_position = $PlayerSpawn.position
+	
+	# Instance player
+	var new_player = player_scene.instance()
+	new_player.position = spawn_position
+	
+	# Delay spawning
+	yield(get_tree().create_timer(GHOST_SPAWN_DELAY * ghosts.size()), "timeout")
+	$Objects.add_child(new_player)
+	new_player.setup_and_start(ghost_no)
+	
--- Src/Player/Player.gd
+++ Src/Player/Player.gd
@@ -1,16 +1,91 @@
 extends KinematicBody2D
 
+const DEFAULT_GRAVITY = Vector2(0, 10)
+const JUMP_FORCE = 250
+const STOP_FORCE_FLOOR = 700
+const STOP_FORCE_AIR = 80
+const WALK_FORCE = 1600
+const MAX_SPEED = 140
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var state = {
+	"ghost_no": -1,
+	
+	# Movement
+	"velocity": Vector2(0,0),
+	"jumping": false,
+	"has_jumped": false,
+	"air_time": 0,
+	
+}
+
+func is_ghost():
+	return false if state.ghost_no == -1 else true
+
+func setup_and_start(ghost_no):
+	state.ghost_no = ghost_no
+
+func _physics_process(delta):
+	if is_ghost():
+		# Ghost
+		pass
+	else:
+		# Player
+		process_movement(delta)
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func process_movement(delta):
+	var input_direction = get_direction_input()
+	var on_floor_or_ghost = is_on_floor_or_ghost()
+	
+	# Jumping
+	if on_floor_or_ghost:
+		# Reset Jump state
+		state.jumping = false
+		state.has_jumped = false
+		state.air_time = 0
+	else:
+		state.air_time += 1 # TODO: use delta
 
+	if Input.is_action_just_pressed('ui_jump') and on_floor_or_ghost and not state.jumping:
+		state.velocity.y =- JUMP_FORCE
+		state.jumping = true
+		state.has_jumped = true
+	
+	# Stop movement
+	if input_direction == Vector2(0.0, 0.0):
+		var stop_force = delta
+		if on_floor_or_ghost:
+			stop_force *= STOP_FORCE_FLOOR
+		else:
+			stop_force *= STOP_FORCE_AIR
+			
+		if state.velocity.x > 0:
+			state.velocity.x = max(state.velocity.x - stop_force, 0)
+		elif state.velocity.x < 0:
+			state.velocity.x = min(state.velocity.x + stop_force, 0)
+	# Accelarete
+	else:
+		state.velocity.x += input_direction.x * delta * WALK_FORCE
+		if state.velocity.x > MAX_SPEED:
+			state.velocity.x = MAX_SPEED
+		elif state.velocity.x < -MAX_SPEED:
+			state.velocity.x = -MAX_SPEED
+	
+	state.velocity.y += DEFAULT_GRAVITY.y
+	state.velocity = move_and_slide(state.velocity, Vector2(0, -1))
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func is_on_floor_or_ghost():
+	var collision = is_on_floor()
+	
+	if not collision:
+		# TODO: Check for ghost collision here
+		pass
+
+	return collision
+	
+
+func get_direction_input():
+	var input = Vector2(0, 0)
+	input.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
+	input.y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
+	return input
